be91c298d9ef72fe45dbbfd318ec299b897edca1
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2026-02-16 12:54:56 +0000

    Deploying to gh-pages from @ ceciDucos/therian-community@7d7157df8cf18c52632f453dc997879f42da012f 🚀

---
+++ chunk-A7VTSCNG.js
@@ -0,0 +1,1 @@
+var e={production:!0,supabaseUrl:"https://wjoxqfgpqlojgehjzxyp.supabase.co",supabaseKey:"sb_publishable_YHvO95ATilSjlb_BNA-zgQ_3m2cCgTz",mmoUrl:"https://therian-mmo-api.onrender.com"};export{e as a};
