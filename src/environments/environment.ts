b527f1e66e378287494f3e06d2f5e532c24df96b
Cecilia Ducos <cecilia.ducos@progleasing.com>
2026-02-16 09:21:41 -0300

    Configure production: API URL and base-href

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     supabaseUrl: 'https://wjoxqfgpqlojgehjzxyp.supabase.co',
     supabaseKey: 'sb_publishable_YHvO95ATilSjlb_BNA-zgQ_3m2cCgTz',
-    mmoUrl: 'http://localhost:3000'
+    mmoUrl: 'https://therian-mmo-api.onrender.com'
 };
